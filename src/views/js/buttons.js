9a2ac3ad41a54046ebeca86725d19db36b5f025d
Jesse Bryan <winneonsword@gmail.com>
2015-08-10 18:20:32 -0500

    Added CSS rules for modals and added some login code.

--- src/views/js/buttons.js
+++ src/views/js/buttons.js
@@ -1,4 +1,5 @@
 function Buttons(){
+	// navigation bar buttons
 	$("a, li").on("click", function(event){
 		var $this = $(this),
 		    modal = $this.attr("modal");
@@ -6,9 +7,34 @@ function Buttons(){
 			utils.modal(modal);
 		}
 	});
-	
-	if (logged_in){
 
+	if (logged_in){
+		$("li[name='search']").on("click", function(event){
+
+		});
+
+		$("li[name='drop']").on("click", function(event){
+			
+		});
+
+		$("li[name='veto']").on("click", function(event){
+			
+		});
+	}
+
+	// modal buttons
+
+	if (logged_in){
+		// TODO place add modal event handlers here
+	} else {
+		$("input.username, input.password").on("keydown", function(event){
+			if (event.which == 13){
+				utils.login();
+			}
+		});
+		$("button[name='login']").on("click", function(event){
+			utils.login();
+		});
 	}
 
 	return this;
--- src/views/js/utils.js
+++ src/views/js/utils.js
@@ -1,14 +1,29 @@
 function Utils(){
 	this.modal = function(name){
-		if ($("div.modal").length > 0 && $("div.modal.modal-" + name).length > 0){
+		var that = this;
+
+		if ($("div.modal").length > 0 &&
+		    $("div.modal.modal-" + name).length > 0 &&
+		    $("div.modal.visible").length == 0){
 			$("div.wrapper").addClass("modal");
 			$("div.screen, div.modal.modal-" + name).addClass("visible");
 
 			$("div.screen").on("click", function(event){
-				$("div.wrapper").removeClass("modal");
-				$("div.screen, div.modal.modal-" + name).removeClass("visible");
+				that.clear_modal(name);
 			});
 		}
+	};
+
+	this.clear_modal = function(name){
+		$("div.wrapper").removeClass("modal");
+		$("div.screen, div.modal.modal-" + name).removeClass("visible");
+	};
+
+	this.login = function(){
+		socket.emit("login", {
+			username: $("input.username").val(),
+			password: $("input.password").val()
+		});
 	};
 
 	return this;
