832a373e679dbe715fd0116de6db302f4e1f717c
Jesse Bryan <winneonsword@gmail.com>
2015-08-12 01:20:29 -0500

    Fixed a blatant typo that makes me feel like an idiot.

--- src/views/js/utils.js
+++ src/views/js/utils.js
@@ -8,13 +8,11 @@ function Utils(){
 			$("div.wrapper").addClass("modal");
 			$("div.screen, div.modal.modal-" + name).addClass("visible");
 
+			$("div.modal.modal-" + name).find("input").focus();
+
 			$("div.screen").on("click", function(event){
 				that.clear_modal(name);
 			});
-
-			setTimeout(function(){
-				$("dv.modal.modal-" + name).find("input").focus();
-			}, 2000);
 		}
 	};
 
